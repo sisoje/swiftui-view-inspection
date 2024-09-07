5bca8e5218fa9b062f08d10d7b6fbca8739d611e
Lazar Otasevic <>
2024-09-08 00:26:06 +0400

    added gestures

--- Sources/ViewInspection/Elements/TestElement.swift
+++ Sources/ViewInspection/Elements/TestElement.swift
@@ -457,6 +457,76 @@ enum TestElement {
         typealias _ZStack = SameBaseElement<ZStack<Never>>
     }
     
+    enum Gesture {
+        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+        typealias _AnyGesture = SameBaseElement<AnyGesture<Never>>
+
+        @available(iOS 13.0, macOS 10.15, watchOS 6.0, *)
+        @available(tvOS, unavailable)
+        typealias _DragGesture = SameTypeElement<DragGesture>
+
+        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+        typealias _ExclusiveGesture = SameBaseElement<ExclusiveGesture<Never,Never>>
+
+        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+        typealias _GestureStateGesture = SameBaseElement<GestureStateGesture<Never,Never>>
+
+        @available(iOS 13.0, macOS 10.15, watchOS 6.0, tvOS 14.0, *)
+        typealias _LongPressGesture = SameTypeElement<LongPressGesture>
+
+        @available(iOS, introduced: 13.0, deprecated: 100000.0, renamed: "MagnifyGesture")
+        @available(macOS, introduced: 10.15, deprecated: 100000.0, renamed: "MagnifyGesture")
+        @available(watchOS, unavailable)
+        @available(tvOS, unavailable)
+        @available(visionOS, introduced: 1.0, deprecated: 100000.0, renamed: "MagnifyGesture")
+        typealias _MagnificationGesture = SameTypeElement<MagnificationGesture>
+
+        @available(iOS 17.0, macOS 14.0, *)
+        @available(watchOS, unavailable)
+        @available(tvOS, unavailable)
+        typealias _MagnifyGesture = SameTypeElement<MagnifyGesture>
+
+        @available(iOS 17.0, macOS 14.0, *)
+        @available(watchOS, unavailable)
+        @available(tvOS, unavailable)
+        typealias _RotateGesture = SameTypeElement<RotateGesture>
+
+        @available(iOS, introduced: 13.0, deprecated: 100000.0, renamed: "RotateGesture")
+        @available(macOS, introduced: 10.15, deprecated: 100000.0, renamed: "RotateGesture")
+        @available(tvOS, unavailable)
+        @available(watchOS, unavailable)
+        @available(visionOS, introduced: 1.0, deprecated: 100000.0, renamed: "RotateGesture")
+        typealias _RotationGesture = SameTypeElement<RotationGesture>
+
+        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+        typealias _SequenceGesture = SameBaseElement<SequenceGesture<Never,Never>>
+
+        @available(iOS 13.0, macOS 10.15, tvOS 13.0, watchOS 6.0, *)
+        typealias _SimultaneousGesture = SameBaseElement<SimultaneousGesture<Never,Never>>
+
+        #if swift(>=6.0)
+        @available(visionOS 1.0, iOS 18.0, macOS 15.0, watchOS 11.0, *)
+        @available(tvOS, unavailable)
+        typealias _SpatialEventGesture = SameTypeElement<SpatialEventGesture>
+        #endif
+
+        @available(iOS 16.0, macOS 13.0, watchOS 9.0, visionOS 1.0, *)
+        @available(tvOS, unavailable)
+        typealias _SpatialTapGesture = SameTypeElement<SpatialTapGesture>
+
+        @available(iOS 13.0, macOS 10.15, tvOS 16.0, watchOS 6.0, *)
+        typealias _TapGesture = SameTypeElement<TapGesture>
+
+        #if swift(>=6.0)
+        @available(macOS 15.0, *)
+        @available(iOS, unavailable)
+        @available(watchOS, unavailable)
+        @available(tvOS, unavailable)
+        @available(visionOS, unavailable)
+        typealias _WindowDragGesture = SameTypeElement<WindowDragGesture>
+        #endif
+    }
+    
     enum Modifier {
         @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
         struct _Refreshable: ModifierDerivedElement {
@@ -520,6 +590,12 @@ extension TestElement.Modifier._Task {
 
 extension TestElement.Modifier._OnAppear {
     func doOnAppear() {
+        node.oneElement(TestElement.Value._closure.self).castValue()
+    }
+}
+
+extension TestElement.Modifier._OnDisappear {
+    func doOnDisappear() {
         node.oneElement(TestElement.Value._closure.self).castValue()
     }
 }
--- Tests/ViewInspectionTests/ModifierElementsTests.swift
+++ Tests/ViewInspectionTests/ModifierElementsTests.swift
@@ -1,6 +1,6 @@
 import SwiftUI
-import XCTest
 @testable import ViewInspection
+import XCTest
 
 final class ModifierElementsTests: XCTestCase {}
 
@@ -8,27 +8,47 @@ final class ModifierElementsTests: XCTestCase {}
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func test_Refreshable() async {
         var x = 0
-        await EmptyView().refreshable { x = 1 }.snapshot.oneElement(TestElement.Modifier._Refreshable.self).doRefresh()
+        let v = EmptyView().refreshable { x = 1 }.snapshot
+        XCTAssertEqual(v.allNodes.count, 4)
+        await v.oneElement(TestElement.Modifier._Refreshable.self).doRefresh()
         XCTAssert(x == 1)
     }
 
     @available(iOS 15.0, macOS 12.0, tvOS 15.0, watchOS 8.0, *)
     func test_Task() async {
         var x = 0
-        await EmptyView().task { x = 1 }.snapshot.oneElement(TestElement.Modifier._Task.self).doTask()
+        let v = EmptyView().task { x = 1 }.snapshot
+        XCTAssertEqual(v.allNodes.count, 6)
+        await v.oneElement(TestElement.Modifier._Task.self).doTask()
         XCTAssert(x == 1)
     }
 
     func test_OnAppear() {
         var x = 0
-        EmptyView().onAppear { x = 1 }.snapshot.oneElement(TestElement.Modifier._OnAppear.self).doOnAppear()
+        let v = EmptyView().onAppear { x = 1 }.snapshot
+        XCTAssertEqual(v.allNodes.count, 6)
+        v.oneElement(TestElement.Modifier._OnAppear.self).doOnAppear()
+        XCTAssert(x == 1)
+    }
+    
+    func test_OnDisappear() {
+        var x = 0
+        let v = EmptyView().onDisappear { x = 1 }.snapshot
+        XCTAssertEqual(v.allNodes.count, 6)
+        v.oneElement(TestElement.Modifier._OnDisappear.self).doOnDisappear()
         XCTAssert(x == 1)
     }
 
     @available(iOS 13.0, macOS 10.15, tvOS 16.0, watchOS 6.0, *)
     func test_OnTap() {
         var x = 0
-        EmptyView().onTapGesture { x = 1 }.snapshot.oneElement(TestElement.Modifier._OnTap.self).doTap()
+        let v = EmptyView().onTapGesture { x = 1 }.snapshot
+        XCTAssertEqual(v.allNodes.count, 13)
+        v.oneElement(TestElement.Modifier._OnTap.self).doTap()
         XCTAssert(x == 1)
+        XCTAssertEqual(
+            v.allElements(TestElement.Gesture._TapGesture.self).count,
+            1
+        )
     }
 }
