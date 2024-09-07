4d8dd8eb5c37ff2da76faa750c2d36e9f52b86d7
Lazar Otasevic <>
2024-09-07 18:03:46 +0400

    sredio

--- apple.py
+++ apple.py
@@ -8,48 +8,46 @@ identifiers = doc['relationshipsSections'][1]['identifiers']
 views = [x.split("/")[-1] for x in identifiers]
 
 with open('parsed.json', 'r') as file:
-	structs = json.load(file)
+    structs = json.load(file)
     
 preferedTypes = {
-	'View' : 'Never',
-	'Shape': 'Circle',
-	'InsettableShape': 'Circle',
-	'Hashable': 'Int',
-	'RandomAccessCollection': 'Array<Never>',
-	'Publisher': 'AnyPublisher<Void,Never>',
-	'TimelineSchedule': 'AnimationTimelineSchedule',
+    'View' : 'Never',
+    'Shape': 'Circle',
+    'InsettableShape': 'Circle',
+    'Hashable': 'Int',
+    'RandomAccessCollection': 'Array<Never>',
+    'Publisher': 'AnyPublisher<Void,Never>',
+    'TimelineSchedule': 'AnimationTimelineSchedule',
 }
-
-structs = sorted(structs, key=lambda x: x['name'].lower())
 
 output = []
 for struct in structs:
-	name = struct['name']
+    name = struct['name']
 
-	if not name in views:
-		continue
+    if not name in views:
+        continue
 
-	lines = struct.get('availabilities', [])
-	generics = struct.get('generics')
-	if generics is None:
-		t = f"typealias _{name} = SameTypeElement<{name}>"
-	else:
-		conditions = struct.get('conditions', {})
-		arr = [conditions.get(gen, 'Never') for gen in generics]
-		arr = [preferedTypes.get(x, 'Never') for x in arr]
-		gen = ",".join(arr)
-		t = f"typealias _{name} = SameBaseElement<{name}<{gen}>>"       
+    lines = struct.get('availabilities', [])
+    generics = struct.get('generics')
+    if generics is None:
+        t = f'typealias _{name} = SameTypeElement<{name}>'
+    else:
+        conditions = struct.get('conditions', {})
+        arr = [conditions.get(gen, ['Never'])[-1] for gen in generics]
+        arr = [preferedTypes.get(x, 'Never') for x in arr]
+        gen = ",".join(arr)
+        t = f"typealias _{name} = SameBaseElement<{name}<{gen}>>"       
 
-	lines.append(t)
+    lines.append(t)
 
-	output.append(lines)
+    output.append(lines)
 
 strings = ["\n".join(lines) for lines in output]
 
 content = "\n\n".join(strings)
 
 with open('generics.swift', 'w', encoding='utf-8') as f:
-	f.write(content)
+    f.write(content)
 
 
 
--- parser2.py
+++ parser2.py
@@ -1,41 +1,44 @@
 import re
 import json
 
+
 def parse_file(input_file):
-	try:
-		output = []
-		with open(input_file) as f:
-			lines = [line.rstrip() for line in f]
+    try:
+        output = []
+        with open(input_file) as f:
+            lines = [line.rstrip() for line in f]
 
-			for idx, line in enumerate(lines):
-				if not line.startswith(" ") and "public struct" in line:
-					linije = []
-					i = idx - 1
-					while lines[i].startswith('@'):
-						if lines[i].startswith('@available'):
-							linije.insert(0, lines[i])
-						i -= 1
+            for idx, line in enumerate(lines):
+                if not line.startswith(" ") and "public struct" in line:
+                    linije = []
+                    i = idx - 1
+                    while lines[i].startswith('@'):
+                        if lines[i].startswith('@available'):
+                            linije.insert(0, lines[i])
+                        i -= 1
 
-					output.append({
-						'struct': line,
-						'availabilities': linije
-					})
-		return output
-	except:
-		return []
+                    output.append({
+                        'struct': line,
+                        'availabilities': linije
+                    })
+        return output
+    except:
+        return []
+
 
 def parse_generic_struct(line):
     # Regex to capture generic structs with conformances
-    generic_struct_pattern = r'public struct (\w+)<([^>]+)>\s+(?:\s*:\s*([^{]+))?'
+    generic_struct_pattern = r'public struct (\w+)<([^>]+)>(?:\s*:\s*([^{]+))?'
     
     match = re.search(generic_struct_pattern, line)
     if match:
         struct_name = match.group(1)
         generics = match.group(2)
         conformances = match.group(3).split(', ') if match.group(3) else []
-        conformances = [x.split(".")[1] for x in conformances]
+        conformances = [x.split(".")[1].strip() for x in conformances]
         return { 'name': struct_name, 'generics': generics.split(', '), 'conformances': conformances }
     return None
+
 
 def parse_non_generic_struct(line):
     # Regex to capture non-generic structs with conformances
@@ -45,9 +48,10 @@ def parse_non_generic_struct(line):
     if match:
         struct_name = match.group(1)
         conformances = match.group(2).split(', ') if match.group(2) else []
-        conformances = [x.split(".")[1] for x in conformances]
+        conformances = [x.split(".")[1].strip() for x in conformances]
         return { 'name': struct_name, 'conformances': conformances }
     return None
+
 
 def parse_swift_generic_conditions(declaration):
     # Extract the part after 'where'
@@ -61,17 +65,20 @@ def parse_swift_generic_conditions(declaration):
     condition_list = [c.strip() for c in conditions.split(',')]
     
     parsed_conditions = {}
-    parsed_types = {}
-    for condition in condition_list:
-        if ':' in condition:
-            # Protocol conformance or class inheritance
-            left, right = condition.split(':', 1)
-            right = right.strip().split('.')[1]
-            type = left.strip()
-            parsed_conditions[type] = right
-            parsed_types[type] = '==' if '==' in condition else ':'
+    for conditionx in condition_list:
+        condition = conditionx.replace("==", ":")
+      
+        # Protocol conformance or class inheritance
+        left, right = condition.split(':', 1)
+        right = right.strip().split('.')[1]
+        type = left.strip()
+
+        oldarr = parsed_conditions.get(type, [])
+        oldarr.append(right)
+        parsed_conditions[type] = oldarr
     
-    return {'conditions':parsed_conditions, 'condition_relations':parsed_types }
+    return { 'conditions': parsed_conditions }
+
 
 interface0 = '/Applications/Xcode.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/SwiftUI.framework/Modules/SwiftUI.swiftmodule/arm64-apple-ios.swiftinterface'
 interface1 = '/Applications/Xcode-beta.app/Contents/Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS.sdk/System/Library/Frameworks/SwiftUI.framework/Modules/SwiftUI.swiftmodule/arm64-apple-ios.swiftinterface'
@@ -82,76 +89,22 @@ results = parse_file(interface0) + parse_file(interface1) + parse_file(interface
 parsed_results = []
 
 for result in results:
-	line = result['struct']
-	levo = line.split('where', 1)[0]
-	parsed_struct = parse_non_generic_struct(levo)
-	if parsed_struct is None:
-		parsed_struct = parse_generic_struct(levo)
+    line = result['struct']
+    levo = line.split('where', 1)[0]
+    parsed_struct = parse_non_generic_struct(levo)
+    if parsed_struct is None:
+        parsed_struct = parse_generic_struct(levo)
 
-	dic = result
-	dic.update(parsed_struct)
-	
-	parsed_conditions = parse_swift_generic_conditions(line)
-	if parsed_conditions is not None:
-		dic.update(parsed_conditions)
+    dic = result
+    dic.update(parsed_struct)
+    
+    parsed_conditions = parse_swift_generic_conditions(line)
+    if parsed_conditions is not None:
+        dic.update(parsed_conditions)
 
-	parsed_results.append(dic)
+    parsed_results.append(dic)
+
+parsed_results = sorted(parsed_results, key=lambda x: x['name'].lower())
 
 with open(parsed, 'w', encoding='utf-8') as f:
-	json.dump(parsed_results, f, ensure_ascii=False, indent=4)
-
-exit(0)
-mapping = {
-	"SwiftUICore.Shape": "Circle",
-	"SwiftUICore.View": "AnyView",
-	"Swift.Hashable": "Int"
-}
-
-def transform(struct):
-	# print(struct)
-	generic_struct_pattern = r'public struct (\w+)<([^>]+)>'
-	# reg = "public\s+struct\s+(\w+)\s*<([^>]+)>\s*:\s*([\w.]+)"
-	m = re.search(r"public struct (\w+)([^>]+)>\s*:\s*([\w.]+)", struct)
-	if m is None:
-		return None
-	if m.group(3) != "SwiftUICore.View":
-		return None
-	sname = m.group(1)
-	gener = m.group(2)
-
-	parts = struct.split(' where ', 1)
-	values = [item for item in parts[-1].split(', ')]
-
-	dic = {}
-	for val in values:
-		infos = re.split(r'\s:\s|\s==\s', val)
-		dic[infos[0]] = infos[1]
-
-	keys = gener.split(", ")
-
-	c = {}
-	g2 = []
-	for key in keys:
-		t1 = dic.get(key, "N/A")
-		t = mapping.get(t1, "Never")
-		c[key] = t
-		g2.append(t)
-
-	x = ", ".join(g2)
-	return f"typealias _{sname} = SameBaseElement<{sname}<{x}>>"
-
-var = []
-for r in results:
-	str = r[-1]
-	t = transform(str)
-	if t is None:
-		continue
-	nr = r
-	nr[-1] = t
-	t = "\n".join(nr)
-	var.append(t)
-
-cont = "\n\n".join(var)
-
-print(cont)
-
+    json.dump(parsed_results, f, ensure_ascii=False, indent=4)
