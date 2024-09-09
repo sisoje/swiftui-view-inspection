08ed7e9224ac50f8cbd18270a921431f934bd890
Lazar Otasevic <>
2024-09-10 02:03:57 +0400

    some test

--- Tests/ViewInspectionTests/Elements/_VStack.swift
+++ Tests/ViewInspectionTests/Elements/_VStack.swift
@@ -5,6 +5,6 @@ import XCTest
 @MainActor final class Test_VStack: XCTestCase {
     func test_VStack() throws {
         let vstack = try VStack { Text("a") }.snap.one(.vStack)
-        XCTAssertEqual(try vstack.content.one(.text).string, "a")
+        XCTAssertEqual(try vstack.node.one(.text).string, "a")
     }
 }
