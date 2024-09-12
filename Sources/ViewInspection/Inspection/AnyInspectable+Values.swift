dbaf5efaa05899a225e874a5732e6c614bb8f13e
Lazar Otasevic <>
2024-09-12 23:59:09 +0400

    ttt

--- Sources/ViewInspection/Inspection/AnyInspectable+Values.swift
+++ Sources/ViewInspection/Inspection/AnyInspectable+Values.swift
@@ -1,4 +1,8 @@
 extension AnyInspectable {
+    static func sameType<E>(_ t: E.Type = E.self) -> AnyInspectable<SameTypeElement<E>> {
+        .some
+    }
+
     static var closure: AnyInspectable<ClosureElement<() -> Void>> { .some }
     static var closureWithVoidParam: AnyInspectable<ClosureElement<(()) -> Void>> { .some }
     static var asyncClosure: AnyInspectable<ClosureElement<() async -> Void>> { .some }
